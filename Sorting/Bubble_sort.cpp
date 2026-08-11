8f1dece5906abd479388516f61027a228803dc9e
SohamPatil <sohamanilpatil@gmail.com>
2026-08-11 20:45:17 +0530

    Add Bubble Sort implementation

---
+++ Sorting/Bubble_sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+
+    for (int i = 1; i < n; i++) {
+
+        for (int j = 0; j < n - i; j++) {
+
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+
+        }
+    }
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    // Input array
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Call bubble sort
+    bubble_sort(arr, n);
+
+    // Print sorted array
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
